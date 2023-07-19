782859903554650a83f9f7ad2fb49742143d795e
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2023-07-19 23:36:10 +0530

    Added robot publish description and semantic

--- mit_robot_moveit_config/launch/mit_robot_moveit.launch.py
+++ mit_robot_moveit_config/launch/mit_robot_moveit.launch.py
@@ -222,6 +222,8 @@ def launch_setup(context, *args, **kwargs):
         "publish_geometry_updates": True,
         "publish_state_updates": True,
         "publish_transforms_updates": True,
+        "publish_robot_description": True,
+        "publish_robot_description_semantic": True,
     }
 
     warehouse_ros_config = {
